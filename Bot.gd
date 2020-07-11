2481d49c96e84b2274e545ccbb3aa4270295abd0
Eilam Gilboa <eilam.gilboa@gmail.com>
2020-07-11 14:32:51 +0300

    Bot now obeys gravity and can ride elevators

--- Bot.gd
+++ Bot.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 
 export var active = false
 export var speed = 1000
+export var gravity = 100
+
+var movement = Vector2.ZERO
+var in_elevator = false
 
 
 func _ready():
@@ -10,19 +14,37 @@ func _ready():
 
 
 func _physics_process(delta):
-    var movement = Vector2.ZERO
+    movement.x = 0
     if active:
         if Input.is_action_pressed("move_left"):
-            movement += Vector2(-speed, 0)
+            movement.x -= speed
         if Input.is_action_pressed("move_right"):
-            movement += Vector2(speed, 0)
-        if Input.is_action_pressed("move_up"):
-            movement += Vector2(0, -speed)
-        if Input.is_action_pressed("move_down"):
-            movement += Vector2(0, speed)
-
+            movement.x += speed
+        if in_elevator:
+            movement.y = 0
+            if Input.is_action_pressed("move_up"):
+                movement.y = -speed
+            if Input.is_action_pressed("move_down"):
+                movement.y = speed
+    
+    if not(in_elevator):
+        movement.y += gravity
+    
+    if movement.x < 0:
+        $Sprite.flip_h = true
+    if movement.x > 0:
+        $Sprite.flip_h = false
+    
     move_and_slide(movement)
 
 
 func _on_character_switched():
     active = not(active)
+
+
+func _on_entered_elevator(body):
+    in_elevator = true
+
+
+func _on_exited_elevator(body):
+    in_elevator = false
