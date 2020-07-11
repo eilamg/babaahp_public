0ab3da9852b082ce001af130104fba97d0aa9fe6
Eilam Gilboa <eilam.gilboa@gmail.com>
2020-07-11 20:05:39 +0300

    Animate scientist

--- Scientist.gd
+++ Scientist.gd
@@ -8,6 +8,7 @@ export var jump_force = 1000
 
 var movement = Vector2.ZERO
 var can_jump = true
+var is_jumping = false
 
 
 func _physics_process(delta):
@@ -18,21 +19,29 @@ func _physics_process(delta):
         if Input.is_action_pressed("move_right"):
             movement.x += speed
         if can_jump and Input.is_action_pressed("move_up"):
+            is_jumping = true
             movement.y = -jump_force
+            $AnimatedSprite.play("jump")
             can_jump = false
 
     movement.y += gravity
     movement.y = clamp(movement.y, -INF, 10000)
     
-    if movement.x < 0:
-        $Sprite.flip_h = true
-    if movement.x > 0:
-        $Sprite.flip_h = false
-    
+    if not(is_jumping):
+        if movement.x < 0:
+            $AnimatedSprite.play("run")
+            $AnimatedSprite.flip_h = true
+        if movement.x > 0:
+            $AnimatedSprite.play("run")
+            $AnimatedSprite.flip_h = false
+        if movement.x == 0:
+            $AnimatedSprite.play("idle")
+            
     move_and_slide(movement, Vector2.UP)
     
     if is_on_floor():
         can_jump = true
+        is_jumping = false
 
 
 func _on_character_switched():
