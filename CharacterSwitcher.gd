4d4f2f9b81d38ee6c5bd30ec0734d1f1b81259d2
Eilam Gilboa <eilam.gilboa@gmail.com>
2020-07-11 13:34:15 +0300

    Fix character switching input

--- CharacterSwitcher.gd
+++ CharacterSwitcher.gd
@@ -5,5 +5,5 @@ signal character_switched
 
 
 func _unhandled_input(event):
-    if Input.is_action_pressed("switch_character"):
+    if Input.is_action_just_pressed("switch_character"):
         emit_signal("character_switched")
