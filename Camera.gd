117234d30f02492f2edb72c1cf6cdbe58e044ffd
Eilam Gilboa <eilam.gilboa@gmail.com>
2020-07-11 22:42:07 +0300

    Add camera shake

---
+++ Camera.gd
@@ -0,0 +1,43 @@
+# Heavily based on original code by KidsCanCode
+# https://kidscancode.org/godot_recipes/2d/screen_shake/
+extends Camera2D
+
+
+export var decay = 0.8  # How quickly the shaking stops [0, 1].
+export var max_offset = Vector2(100, 50)  # Maximum hor/ver shake in pixels.
+export var max_roll = 0.1  # Maximum rotation in radians (use sparingly).
+
+
+var trauma = 0.0  # Current shake strength.
+var trauma_power = 2  # Trauma exponent. Use [2, 3].
+onready var noise = OpenSimplexNoise.new()
+var noise_y = 0
+
+
+func _ready():
+    randomize()
+    noise.seed = randi()
+    noise.period = 4
+    noise.octaves = 2
+
+
+func _process(delta):
+    if trauma:
+        trauma = max(trauma - decay * delta, 0)
+        shake()
+
+        
+func shake():
+    var amount = pow(trauma, trauma_power)
+    noise_y += 1
+    rotation = max_roll * amount * noise.get_noise_2d(noise.seed, noise_y)
+    offset.x = max_offset.x * amount * noise.get_noise_2d(noise.seed*2, noise_y)
+    offset.y = max_offset.y * amount * noise.get_noise_2d(noise.seed*3, noise_y)
+
+
+func add_trauma(amount):
+    trauma = min(trauma + amount, 1.0)
+
+
+func _on_Crusher_ground_hit():
+    add_trauma(1)
--- Crusher.gd
+++ Crusher.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 signal killbox_hit
+signal ground_hit
 
 
 func crush():
@@ -15,3 +16,7 @@ func _on_animation_finished(anim_name):
 
 func _on_Killbox_body_entered(body):
     emit_signal("killbox_hit", body)
+
+
+func _on_hit_ground():
+    emit_signal("ground_hit")
